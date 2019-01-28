16c8a2a2a76511b0b8c0e675cd89ac61a2aab839
Joshua D Rodriguez <jdrodrig@jpl.nasa.gov>
2019-01-28 14:18:48 -0800

    (Partially?) fixes #3 (links opening in same window and not new tab).

--- client/src/algorithms/Cluster/Help/Help.js
+++ client/src/algorithms/Cluster/Help/Help.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import ReactModal from 'react-modal';
-import ReactMarkdown from 'react-markdown';
-import { MdClose } from 'react-icons/lib/md';
+import ReactModal from "react-modal";
+import ReactMarkdown from "react-markdown";
+import { MdClose } from "react-icons/lib/md";
 
-import './Help.css';
+import "./Help.css";
 
 class Help extends Component {
   constructor(props) {
@@ -12,44 +12,54 @@ class Help extends Component {
 
     this.state = {
       open: false,
-      markdown: ''  
+      markdown: ""
     };
   }
 
   open() {
-    this.setState( { open: true } );
+    this.setState({ open: true });
   }
   close() {
-    this.setState( { open: false } );
+    this.setState({ open: false });
   }
-  setMarkdown( markdown ) {
-    this.setState( { markdown: markdown } );
+  setMarkdown(markdown) {
+    this.setState({ markdown: markdown });
+  }
+
+  LinkRenderer(props) {
+    return (
+      <a href={props.href} target="_blank">
+        {props.children}
+      </a>
+    );
   }
 
   render() {
     return (
-      <div className='Help'>
-        <ReactModal 
-            portalClassName='ReactModalPortal HelpModal'
-            isOpen={this.state.open}
-            parentSelector={this.props.parent}
-            ariaHideApp={false}
-            closeTimeoutMS={200}
-            style={{
-              overlay: {
-                backgroundColor: 'rgb(255,255,255)'
-              },
-              content: {
-                padding: 0
-              }
-            }}
-          >
-          <div id='helpClose'
-            onClick={ () => this.props.close() }>
+      <div className="Help">
+        <ReactModal
+          portalClassName="ReactModalPortal HelpModal"
+          isOpen={this.state.open}
+          parentSelector={this.props.parent}
+          ariaHideApp={false}
+          closeTimeoutMS={200}
+          style={{
+            overlay: {
+              backgroundColor: "rgb(255,255,255)"
+            },
+            content: {
+              padding: 0
+            }
+          }}
+        >
+          <div id="helpClose" onClick={() => this.props.close()}>
             <MdClose />
           </div>
-          <div id='helpContainer' style={{ padding: '20px 20% 20px 20%' }}>
-            <ReactMarkdown source={this.state.markdown} />
+          <div id="helpContainer" style={{ padding: "20px 20% 20px 20%" }}>
+            <ReactMarkdown
+              source={this.state.markdown}
+              renderers={{ link: this.LinkRenderer }}
+            />
           </div>
         </ReactModal>
       </div>
