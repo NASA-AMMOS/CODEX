31e065363fc1b007143650aa193eda198d8bfe04
Anton Kulikov <akulikov@jpl.nasa.gov>
2019-01-29 10:37:42 -0800

    Updates webpack configuration

---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const paths = require('./config/paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true,
+    inline: true,
+    port: 3000
+  },
+  devtool: "cheap-module-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+    }),
+  ]
+};
