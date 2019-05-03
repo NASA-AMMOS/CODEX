d40051892a9a0ca606836c1b908892a2e1b234f8
Joshua D Rodriguez <jdrodrig@jpl.nasa.gov>
2019-05-03 16:46:24 -0700

    coordinate box off for cluster graphs.

--- client/src/components/Algorithms/SubalgoChart.js
+++ client/src/components/Algorithms/SubalgoChart.js
@@ -48,7 +48,7 @@ function makeChart(props) {
         layout: {
             autosize: true,
             margin: { l: 0, r: 0, t: 0, b: 0 }, // Axis tick labels are drawn in the margin space
-            hovermode: "closest", // Turning off hovermode seems to screw up click handling
+            hovermode: false, // Turning off hovermode seems to screw up click handling
             titlefont: { size: 5 },
             xaxis: {
                 automargin: true
