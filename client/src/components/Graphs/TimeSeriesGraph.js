829b7e83db13f82bf0affac864b606e2497e04ee
Alec Helbling <alec.f.helbling@jpl.nasa.gov>
2019-05-20 17:06:53 -0700

    Working prototype of TimeSeriesGraph now I just need to make zooming synchonized and selection work.

--- client/src/components/Graphs/TimeSeriesGraph.js
+++ client/src/components/Graphs/TimeSeriesGraph.js
@@ -9,6 +9,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import Plot from "react-plotly.js";
+import Plotly from 'plotly.js';
 import * as utils from "utils/utils";
 import ReactResizeDetector from "react-resize-detector";
 
@@ -27,8 +28,8 @@ function generatePlotData(features) {
         data[i] = {
             x: timeAxis,
             y: features[i],
-            xaxis: 'x'+(i+1),
-            yaxis: 'y'+(i+1),
+            xaxis: 'x',
+            yaxis: 'y1',
             type: "scattergl",
             mode: "lines",
             visible: true,
@@ -44,8 +45,8 @@ function generateLayouts(features) {
     for(let index = 0; index < features.length; index++) {
         let layout = {
             autosize: true,
-            margin: { l: 15, r: 5, t: 5, b: 20 }, // Axis tick labels are drawn in the margin space
-            dragmode: 'lasso',
+            margin: { l: 0, r: 5, t: 0, b: 0 }, // Axis tick labels are drawn in the margin space
+            dragmode: "select",
             hovermode: "compare", // Turning off hovermode seems to screw up click handling
             titlefont: { size: 5 },
             xaxis: {
@@ -54,7 +55,7 @@ function generateLayouts(features) {
             yaxis: {
                 automargin: true,
                 fixedrange: true,
-                showline: false,
+                showline: false
             }
         };
 
@@ -63,7 +64,6 @@ function generateLayouts(features) {
             layout.xaxis.visible = false;
             layout.margin.l = 20;
         }
-        console.log(features[index][0]);
         //add axis title
         layout.yaxis.title = {
             text:features[index][0]
@@ -94,6 +94,31 @@ function TimeSeriesGraph(props) {
 
     let layouts = generateLayouts(features);
 
+    let subPlots = data.map((dataElement,index) => (
+        <TimeSeriesSubGraph
+            data={dataElement}
+            chart={chartRefs[index]}
+            layout={layouts[index]}
+            globalChartState={props.globalChartState}
+        />
+    ));
+
+    function relayout(ed, divs) {
+        divs.forEach((div, i) => {
+            let x = div.layout.xaxis;
+            if (ed["xaxis.autorange"] && x.autorange) return;
+            if (x.range[0] != ed["xaxis.range[0]"] || x.range[1] != ed["xaxis.range[1]"])
+                Plotly.relayout(div, ed);      
+        });
+    }
+
+    subPlots.forEach((plot) => {
+        window.addEventListener("plotly_relayout", function(ed) {
+            console.log(ed);
+            relayout(ed, subPlots);
+        });
+    });
+
     return (
         <React.Fragment>
             <ReactResizeDetector
@@ -103,16 +128,7 @@ function TimeSeriesGraph(props) {
             />
             <div className="chart-container" onContextMenu={handleContextMenu}>
                 <ul className="time-series-plot-container"> 
-                    {
-                        data.map((dataElement,index) => (
-                            <TimeSeriesSubGraph
-                                data={dataElement}
-                                chart={chartRefs[index]}
-                                layout={layouts[index]}
-                                globalChartState={props.globalChartState}
-                            />
-                        ))
-                    }
+                    {subPlots}
                 </ul>
             </div>
             <Popover
@@ -179,15 +195,6 @@ function TimeSeriesSubGraph(props) {
         },
         [props.currentSelection]
     );
-    //handle this later
-
-    useEffect(
-        _ => {
-            chartState.layout.dragmode = props.globalChartState; // Weirdly this works, can't do it with setChartState
-            updateChartRevision();
-        },
-        [props.globalChartState]
-    );
     
     return (
         <Plot
@@ -204,9 +211,9 @@ function TimeSeriesSubGraph(props) {
                 if (e.event.button === 2) return;
                 props.setCurrentSelection([]);
             }}
-            onSelected={e => {
+            /*onSelected={e => {
                 if (e) props.setCurrentSelection(e.points.map(point => point.pointIndex));
-            }}
+            }}*/
         />
     );
 }
