4eef43f05c6353a41adb8b00677f7e382c5943a6
Anton Kulikov <akulikov@jpl.nasa.gov>
2019-05-23 15:05:01 -0700

    Revert "Makes store availiable to devtools."
    This reverts commit 61e848db7ea6d0c482e3714fee2dbb0f42ca3c95.

--- client/src/store.js
+++ client/src/store.js
@@ -11,14 +11,7 @@ import rootReducer from "reducers";
 export default function configureStore(initialState) {
     const logger = createLogger();
 
-    const store = createStore(
-        rootReducer,
-        initialState,
-        compose(
-            applyMiddleware(thunkMiddleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+    const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunkMiddleware)));
 
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
