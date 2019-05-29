0b63e2a530e0584c755cf399b2ea73f091ae3498
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2019-05-29 13:10:32 -0700

    Adjust tree list

--- server/api/codex_workflow.py
+++ server/api/codex_workflow.py
@@ -135,7 +135,11 @@ def explain_this(inputHash, featureNames, subsetHashName, labelHash, result):
     result['tree_sweep'] = []
 
     samples_, features_ = X.shape
-    for i in range(1, 7):
+    if(features_ < 6):
+    	trees_ = features_
+    else:
+    	trees_ = 6
+    for i in range(1, trees_):
 
         dictionary = {}
         clf = DecisionTreeClassifier(max_features=i)
