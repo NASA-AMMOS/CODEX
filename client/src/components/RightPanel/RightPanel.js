0bcb676496812f03ec288fd148c48e2d9f836b2b
Alec Helbling <alec.f.helbling@jpl.nasa.gov>
2019-06-18 10:43:48 -0700

    Added concept for right context panel

---
+++ client/src/components/RightPanel/RightPanel.js
@@ -0,0 +1,27 @@
+import React, { Component, useState } from "react";
+import "components/RightPanel/RightPanel.scss";
+import Close from "@material-ui/icons/Close";
+import Menu from "@material-ui/icons/menu";
+import IconButton from "@material-ui/core/IconButton";
+
+function RightPanel() {
+    const [open, setOpen] = useState(true);
+
+    return (
+        <React.Fragment>
+            <IconButton className="menu-button-icon" onClick={_ => setOpen(!open)}>
+                <Menu className="menu-button"/>
+            </IconButton>
+            <div className="right-panel" hidden={!open}>
+                <IconButton className="close-button-icon" onClick={_ => setOpen(!open)}>
+                    <Close className="close-button"/>
+                </IconButton>
+                <div className="right-panel-content">
+                    Dummy information for the Right Panel
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default RightPanel;
--- client/src/components/View/View.js
+++ client/src/components/View/View.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 
 import Container from "../Container/Container";
 import LeftPanel from "components/LeftPanel/LeftPanel";
+import RightPanel from "components/RightPanel/RightPanel";
 import TopBar from "components/TopBar/TopBar";
 import ControlBar from "components/ControlBar/ControlBar";
 
@@ -17,6 +18,7 @@ class View extends Component {
                     <ControlBar />
                     <Container />
                 </div>
+                <RightPanel />
             </div>
         );
     }
