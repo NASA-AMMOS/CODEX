830437a29fc0e26376c1257ed0cf80030fcd1225
Patrick Kage <patrick.r.kage@jpl.nasa.gov>
2019-07-17 17:05:14 -0700

    Add some debug info to the debug component

--- client/src/components/Debug/Debug.js
+++ client/src/components/Debug/Debug.js
@@ -1,30 +1,19 @@
 import React from "react";
 import useWindowManager from "hooks/WindowHooks";
-import { usePinnedFeatures, useLiveFeatures } from "hooks/DataHooks";
+import { getGlobalSessionKey } from "utils/utils";
 
-import { WindowError, WindowCircularProgress } from "components/WindowHelpers/WindowCenter";
+import WindowCenter from "components/WindowHelpers/WindowCenter";
 
 const Debugger = props => {
     const win = useWindowManager(props, {
         title: "Debug component"
     });
 
-    const features = usePinnedFeatures();
-
-    let featureList = features;
-    if (features === null) {
-        featureList = (
-            <li>
-                <i> No features loaded </i>
-            </li>
-        );
-    } else {
-        console.log("rendering: ", features);
-        featureList = features.map(f => <li key={f.get("feature")}>{f.get("feature")}</li>);
-    }
-
-    return <WindowCircularProgress />;
-    return <ul>{featureList}</ul>;
+    return (
+        <WindowCenter>
+            <p className="monospace">current session key: {getGlobalSessionKey() || "[unset]"}</p>
+        </WindowCenter>
+    );
 };
 
 export default Debugger;
