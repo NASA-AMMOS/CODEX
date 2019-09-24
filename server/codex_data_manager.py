9f77554d3f1d8c880b6c9416a0ac756e38b168b2
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2019-09-23 18:39:21 -0700

    Added Nones if sentinel values not used

--- server/codex_data_manager.py
+++ server/codex_data_manager.py
@@ -193,7 +193,6 @@ def get_data(msg, nan, inf, ninf, result):
         x_, y_ = data.shape
         data = data.astype(float)
         for x in range(0, x_):
-
             if(np.isnan(data[x][0])):
                 data[x][0] = nan
             elif(np.isinf(data[x][0]) and data[x][0] > 0):
