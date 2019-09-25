06224f53f9e33a5b806836db6b5ab760e31ff8ba
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2019-09-24 22:44:24 -0700

    YAML test now Jenkins compatible

--- server/api/sub/codex_yaml.py
+++ server/api/sub/codex_yaml.py
@@ -11,6 +11,7 @@ import os
 import sys
 import yaml
 
+CODEX_ROOT = os.getenv('CODEX_ROOT')
 sys.path.insert(1, os.getenv('CODEX_ROOT'))
 
 
@@ -21,7 +22,7 @@ def codex_read_yaml(filepath):
     Outputs:
 
     Examples:
-    >>> result = codex_read_yaml('../../guidance.yaml')
+    >>> result = codex_read_yaml(os.path.join(CODEX_ROOT,'guidance.yaml'))
     >>> print(result['unit_tests']['test'])
     This is a unit test
     '''
