22151108408be84f2ad5e57756cdcb395e30f4ba
Joshua D Rodriguez <jdrodrig@jpl.nasa.gov>
2020-02-20 18:45:58 -0800

    Unified controls for Box Plots (#255)

--- client/src/components/Graphs/BoxPlotGraph.js
+++ client/src/components/Graphs/BoxPlotGraph.js
@@ -1,56 +1,85 @@
 import "components/Graphs/BoxPlotGraph.css";
 
 import Plot from "react-plotly.js";
-import React, { useRef, useState, useEffect, createRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-import GraphWrapper, { useBoxSelection } from "components/Graphs/GraphWrapper";
+import GraphWrapper from "components/Graphs/GraphWrapper";
 import * as utils from "utils/utils";
 
 import { filterSingleCol } from "./graphFunctions";
-import { useFeatureDisplayNames } from "../../hooks/DataHooks";
 
 const DEFAULT_POINT_COLOR = "#3386E6";
+const COLOR_CURRENT_SELECTION = "#FF0000";
 const DEFAULT_TITLE = "Box Plot Graph";
 
+function handleGlobalChartState(state) {
+    return state === "lasso" ? "select" : state;
+}
+
 function generateLayouts(features) {
-    let layouts = [];
-
-    for (let index = 0; index < features.length; index++) {
-        let layout = {
-            autosize: true,
-            margin: { l: 15, r: 5, t: 5, b: 20 }, // Axis tick labels are drawn in the margin space
-            dragmode: "select",
-            selectdirection: "v",
-            hovermode: "compare", // Turning off hovermode seems to screw up click handling
-            titlefont: { size: 5 },
-            xaxis: {
-                automargin: true,
-                showline: false
-            },
-            yaxis: {
-                automargin: true,
-                fixedrange: true
-            },
-            shapes: [],
-            modebar: { orientation: "v" }
+    return features.reduce((acc, feature, idx) => {
+        const xAxisName = `xaxis${idx === 0 ? "" : idx + 1}`;
+        const yAxisName = `yaxis${idx === 0 ? "" : idx + 1}`;
+        acc[xAxisName] = {
+            title: feature.feature,
+            automargin: true,
+            showline: false
         };
+        acc[yAxisName] = {
+            automargin: true,
+            fixedrange: true
+        };
+        return acc;
+    }, {});
+}
 
-        layouts.push(layout);
-    }
+function makeSelectionShapes(selection, data) {
+    return data
+        .map((col, dataIdx) =>
+            !selection.rowIndices.length
+                ? null
+                : selection.rowIndices
+                      .reduce((acc, val) => {
+                          const lastSection = acc.length && acc[acc.length - 1];
+                          if (lastSection && lastSection[lastSection.length - 1] + 1 === val) {
+                              lastSection.push(val);
+                              return acc;
+                          }
 
-    return layouts;
+                          acc.push([val]);
+                          return acc;
+                      }, [])
+                      .map(section => {
+                          const xSize = 1 / data.length;
+                          return {
+                              type: "rect",
+                              xref: "paper",
+                              yref: `y${dataIdx > 0 ? dataIdx + 1 : ""}`,
+                              x0: dataIdx * xSize,
+                              y0: section[0],
+                              x1: dataIdx * xSize + xSize,
+                              y1: section[section.length - 1],
+                              fillcolor: selection.color,
+                              opacity: 0.2,
+                              line: {
+                                  // Draw a line when the selection is only a single point
+                                  width: section.length === 1 ? 1 : 0,
+                                  color: selection.color
+                              }
+                          };
+                      })
+        )
+        .flat();
 }
 
 function BoxPlotGraph(props) {
-    const [chartId] = useState(utils.createNewId());
-    //const features = utils.unzip(props.data.get("data"));
     const features = props.data.toJS();
-
     const featureNames = features.map(feature => {
         return feature.feature;
     });
 
-    const chartRefs = useRef(featureNames.map(() => createRef()));
+    const chart = useRef(null);
+    const [chartId] = useState(utils.createNewId());
 
     function generatePlotData() {
         const cols = props.win.data.features
@@ -66,13 +95,18 @@ function BoxPlotGraph(props) {
 
         return [
             features.map((feature, idx) => {
-                return {
+                const trace = {
                     y: cols[idx],
-                    yaxis: "y",
                     type: "box",
-                    visible: true,
-                    name: feature.displayName
+                    hoverinfo: "x+y",
+                    marker: { color: DEFAULT_POINT_COLOR },
+                    name: ""
                 };
+                if (idx > 0) {
+                    trace.xaxis = `x${idx + 1}`;
+                    trace.yaxis = `y${idx + 1}`;
+                }
+                return trace;
             }),
             cols
         ];
@@ -81,7 +115,41 @@ function BoxPlotGraph(props) {
     const [processedData, setProcessedData] = useState(_ => generatePlotData());
     const [data, cols] = processedData;
 
-    let layouts = generateLayouts(features);
+    const layouts = generateLayouts(features);
+
+    // The plotly react element only changes when the revision is incremented.
+    const [chartRevision, setChartRevision] = useState(0);
+
+    const [chartState, setChartState] = useState({
+        data: data,
+        layout: {
+            grid: {
+                rows: 1,
+                columns: data.length,
+                pattern: "independent"
+            },
+            showlegend: false,
+            margin: { l: 40, r: 5, t: 5, b: 20 }, // Axis tick labels are drawn in the margin space
+            dragmode: handleGlobalChartState(props.globalChartState) || "select",
+            selectdirection: "v",
+            hovermode: "compare", // Turning off hovermode seems to screw up click handling
+
+            ...layouts
+        },
+        config: {
+            responsive: true,
+            displaylogo: false
+        }
+    });
+
+    function updateChartRevision() {
+        const revision = chartRevision + 1;
+        setChartState({
+            ...chartState,
+            layout: { ...chartState.layout, datarevision: revision }
+        });
+        setChartRevision(revision);
+    }
 
     const featureDisplayNames = props.win.data.features.map(featureName =>
         props.data.find(feature => feature.get("feature") === featureName).get("displayName")
@@ -89,7 +157,7 @@ function BoxPlotGraph(props) {
 
     // Update bound state with the calculated bounds of the data
     useEffect(_ => {
-        if (!props.win.title) props.win.setTitle(featureDisplayNames.join(", "));
+        if (!props.win.title) props.win.setTitle(featureDisplayNames.join(" , "));
         props.win.setData(data => ({
             ...data.toJS(),
             bounds:
@@ -109,104 +177,67 @@ function BoxPlotGraph(props) {
         [props.win.data.bounds]
     );
 
-    return (
-        <GraphWrapper
-            resizeHandler={_ =>
-                chartRefs.current.forEach(chartRef => chartRef.current.resizeHandler())
-            }
-            chartId={chartId}
-            win={props.win}
-        >
-            <div className="box-plot-container">
-                {data.map((dataElement, index) => (
-                    <BoxPlotSubGraph
-                        key={index}
-                        data={dataElement}
-                        chart={chartRefs.current[index]}
-                        layout={layouts[index]}
-                        globalChartState={props.globalChartState}
-                        setCurrentSelection={props.setCurrentSelection}
-                        currentSelection={props.currentSelection}
-                        savedSelections={props.savedSelections}
-                        chartId={chartId}
-                    />
-                ))}
-            </div>
-        </GraphWrapper>
-    );
-}
-
-function BoxPlotSubGraph(props) {
-    // The plotly react element only changes when the revision is incremented.
-    const [chartRevision, setChartRevision] = useState(0);
-
-    const [chartState, setChartState] = useState({
-        data: [props.data],
-        layout: props.layout,
-        config: {
-            responsive: true,
-            displaylogo: false
-        }
-    });
-
-    let data = chartState.data[0].y;
-
-    function updateChartRevision() {
-        const revision = chartRevision + 1;
-        setChartState({
-            ...chartState,
-            layout: { ...chartState.layout, datarevision: revision }
-        });
-        setChartRevision(revision);
-    }
-
-    const [selectionShapes] = useBoxSelection(
-        "vertical",
-        props.currentSelection,
-        props.savedSelections,
-        chartState.data[0].y
-    );
-
-    useEffect(
-        _ => {
-            chartState.layout.shapes = selectionShapes;
-
-            updateChartRevision();
-        },
-        [selectionShapes]
-    );
-
     // Effect to update graph when data changes
     useEffect(
         _ => {
-            chartState.data = [props.data];
+            chartState.data = data;
             updateChartRevision();
         },
-        [props.data]
+        [data]
+    );
+
+    // Effect to handle drawing of selections
+    useEffect(
+        _ => {
+            chartState.layout.shapes = props.savedSelections
+                .concat(
+                    props.currentSelection.length && {
+                        color: COLOR_CURRENT_SELECTION,
+                        rowIndices: props.currentSelection
+                    }
+                )
+                .filter(x => x)
+                .map(sel => makeSelectionShapes(sel, data))
+                .flat();
+            updateChartRevision();
+        },
+        [props.currentSelection, props.savedSelections]
+    );
+
+    // Update the chart state when the global chart state changes
+    useEffect(
+        _ => {
+            chartState.layout.dragmode = handleGlobalChartState(props.globalChartState);
+            updateChartRevision();
+        },
+        [props.globalChartState]
     );
 
     return (
-        <Plot
-            ref={props.chart}
-            data={chartState.data}
-            layout={chartState.layout}
-            config={chartState.config}
-            style={{ width: "100%", height: "100%" }}
-            useResizeHandler
-            onInitialized={figure => setChartState(figure)}
-            onUpdate={figure => setChartState(figure)}
-            onClick={e => {
-                if (e.event.button === 2 || e.event.ctrlKey) return;
-                props.setCurrentSelection([]);
-            }}
-            onSelected={e => {
-                if (!e) return;
-                let points = utils.indicesInRange(chartState.data[0].y, e.range.y[0], e.range.y[1]);
-
-                props.setCurrentSelection(points);
-            }}
-            divId={props.chartId}
-        />
+        <GraphWrapper chart={chart} win={props.win} chartId={chartId}>
+            <Plot
+                ref={chart}
+                data={chartState.data}
+                layout={chartState.layout}
+                config={chartState.config}
+                style={{ width: "100%", height: "100%" }}
+                onInitialized={figure => setChartState(figure)}
+                onUpdate={figure => setChartState(figure)}
+                useResizeHandler
+                divId={chartId}
+                onClick={e => {
+                    if (e.event.button === 2 || e.event.ctrlKey) return;
+                    props.setCurrentSelection([]);
+                }}
+                onSelected={e => {
+                    if (!e) return;
+                    const selKey = Object.keys(e.range).find(key => key.startsWith("y"));
+                    props.setCurrentSelection(
+                        utils.range(Math.floor(e.range[selKey][0]), Math.ceil(e.range[selKey][1]))
+                    );
+                }}
+            />
+        </GraphWrapper>
     );
 }
 
