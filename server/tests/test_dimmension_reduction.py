7eebee350bff5ea23ce0aeee08e6281227e34884
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2020-03-05 18:27:42 -0800

    Updated unit test framework to new algorithm manager API

--- server/tests/test_dimmension_reduction.py
+++ server/tests/test_dimmension_reduction.py
@@ -20,8 +20,8 @@ def test_dimension_reduction(capsys, testData):
 
     ch = get_cache(DOCTEST_SESSION)
 
-    result = dimension_reduction(testData['inputHash'], testData['hashList'], None, False, "PCA", False, {"n_components":2}, None, "direct", None, {}, ch).run()
+    result = dimension_reduction(testData['featureNames'], testData['inputHash'], testData['hashList'], None, False, "PCA", False, {"n_components":2}, None, "direct", None, {}, ch).run()
     assert result['message'] == 'success'
 
-    result = dimension_reduction(testData['inputHash'], testData['hashList'], None, False, "ICA", False, {"n_components":2}, None, "direct", None, {}, ch).run()
+    result = dimension_reduction(testData['featureNames'], testData['inputHash'], testData['hashList'], None, False, "ICA", False, {"n_components":2}, None, "direct", None, {}, ch).run()
     assert result['message'] == 'success'
