384c6e73cdf3d6b932874c27325a567ec2da291d
Jack Lightholder <jack.a.lightholder@jpl.nasa.gov>
2020-03-19 15:55:05 -0700

    Added server export capabilities

--- server/api/export_manager.py
+++ server/api/export_manager.py
@@ -12,6 +12,7 @@ import sys
 import base64
 import traceback
 import logging
+import numpy as np
 
 sys.path.insert(1, os.getenv('CODEX_ROOT'))
 
@@ -37,8 +38,64 @@ def export_contents(msg, result, savePath):
             outString = "".join(lines)
             outStringEncoded = outString.encode('ascii')
             result['code'] = str(base64.b64encode(outStringEncoded).decode('utf-8'))
+            result['filename'] = 'codex_code.py'
             result['message'] = 'success'
             f.close()
+
+        elif(msg["type"] == "features"):
+            cache = get_cache(msg['sessionkey'])
+            data = cache.return_data()
+            names = []
+            features = []
+            for item in data['features']:
+                names.append(item['name'])
+                features.append(item['data'])
+            if features:
+                features = np.column_stack(features)
+                header = ",".join(names)
+                saveFile = os.path.join(savePath,"features.csv")   
+                np.savetxt(saveFile, features, delimiter=',', header=header)
+                f = open(saveFile, "r")
+                lines = f.readlines()
+                outString = "".join(lines)
+                outStringEncoded = outString.encode('ascii')
+                result['features'] = str(base64.b64encode(outStringEncoded).decode('utf-8'))
+                result['filename'] = 'codex_features.csv'
+                result['content'] = True
+                f.close()
+            else:
+                result['content'] = False
+            result['message'] = 'success'
+
+
+        elif(msg["type"] == "selections"):
+            cache = get_cache(msg['sessionkey'])
+            data = cache.return_data()
+            names = []
+            selections = []
+            for item in data['subsets']:
+                names.append(item['name'])
+                selections.append(item['data'])
+            if selections:
+                selections = np.column_stack(selections)
+                header = ",".join(names)
+                saveFile = os.path.join(savePath,"selections.csv")   
+                np.savetxt(saveFile, selections, delimiter=',', header=header)
+                f = open(saveFile, "r")
+                lines = f.readlines()
+                outString = "".join(lines)
+                outStringEncoded = outString.encode('ascii')
+                result['selections'] = str(base64.b64encode(outStringEncoded).decode('utf-8'))
+                result['filename'] = 'codex_selections.csv'
+                result['content'] = True
+                f.close()
+            else:
+                result['content'] = False
+            result['message'] = 'success'
+        else:
+            result['WARNING'] = 'export type not supported. code|features|selections are supported.'
+            result['message'] = 'failure'
+
 
 
     except:
