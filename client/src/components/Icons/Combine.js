7bc25df2e5d4fea30b1046221da68e46da7e6457
Eyasu Haile <Eyasu.T.Haile@jpl.nasa.gov>
2022-07-07 18:12:31 -0700

    Reinstate: selection math UI (#590)
    * reinstate overwritten selection math ui
    
    * package.json
    
    Co-authored-by: Josh <joshhaile7@gmail.com>

---
+++ client/src/components/Icons/Combine.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CombineIcon(props) {
+    return (
+        <svg viewBox="0 0 22 16" {...props}>
+            <circle cx="8" cy="8" r="8" fill="#E5E5E5" />
+            <circle cx="14" cy="8" r="8" fill="#E5E5E5" />
+        </svg>
+    );
+}
+
+export default CombineIcon;
