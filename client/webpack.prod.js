cddb11e14a29aad44173e6abeab9e987538881b5
Joshua Rodriguez <Joshua.D.Rodriguez@jpl.nasa.gov>
2023-02-02 10:15:33 -0800

    Fixes user guide modal in production build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -3,8 +3,10 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
-    entry: ["./src/index.js"]
+    entry: ["./src/index.js"],
+    plugins: [new CopyWebpackPlugin({ patterns: [{ from: "src/public", to: "public" }] })]
 });
